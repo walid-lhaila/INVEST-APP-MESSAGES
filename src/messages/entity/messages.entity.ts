0bac7933f508fe47407e9d056b6aeed5e836deb9
walid lhaila <walidlhaila00@gmail.com>
2025-02-08 21:10:13 +0100

    Feat: CREATE SCHEMAS AND DTO FOR MESSAGES

---
+++ src/messages/entity/messages.entity.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema()
+export class Messages extends Document {
+  @Prop({ required: true })
+  senderId: string;
+
+  @Prop({ required: true })
+  receiverId: string;
+
+  @Prop({ required: true })
+  content: string;
+
+  @Prop({ default: Date.now })
+  timestamp: Date;
+
+  @Prop({ default: false })
+  seen: boolean;
+}
+export type MessagesDocument = Messages;
+export const MessagesSchema = SchemaFactory.createForClass(Messages);
